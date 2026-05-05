cc6ab8261e386312ea34e74b3260d07b02dca22e
Lukasz Grzedzinski <lukasz.grzedzinski@o2.pl>
2026-05-05 09:55:12 +0200

    Wprowadzanie danych do logowania

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const valid_login = "user1234"
+    const valid_password = "1234"
+    
     let isLoggedIn = false; 
     
     const loginBtn = document.getElementById('loginBtn');
-    const logoutBtn = document.getElementById('logoutBtn');
     const statusText = loginBtn.querySelector('span');
 
     loginBtn.addEventListener('click', () => {
-        // Operator identyczności (strict comparison)
         if (isLoggedIn === false) {
-            isLoggedIn = true;
-            statusText.textContent = "User1234";
-            alert("Zalogowano pomyślnie!");
+            let login = prompt("Podaj login:");
+            let password = prompt("Podaj hasło:");
+
+            if(login === valid_login && password === valid_password) {
+                isLoggedIn = true;
+                statusText.textContent = login;
+                alert(`Pomyślnie zalogowano. Witaj ${login}`);
+            } else {
+                alert("Błędny login lub hasło!");
+            }
         }
     });
 
+    const logoutBtn = document.getElementById('logoutBtn');
     logoutBtn.addEventListener('click', (e) => {
         e.preventDefault();
         
@@ -21,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
             isLoggedIn = false;
             statusText.textContent = "Zaloguj";
             console.log("Użytkownik wylogowany");
+            alert("Wylogowano pomyślnie. Do zobaczenia!");
         }
     });
 });
