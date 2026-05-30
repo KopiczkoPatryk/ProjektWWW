05ba41a8ea4148ce5753b81323b37c46f37f5201
LUKASZ GRZEDZINSKI <lukasz.grzedzinski@o2.pl>
2026-05-30 22:41:12 +0200

    Poprawiono przycinające się zdjęcia produktów i renderowanie body w całości w JavaScript

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-// Przycisk zmiany motywu
-const MotywPrzycisk = document.getElementById("motyw-przycisk")
-function ZmienMotyw() {
-    document.body.classList.toggle("tryb-ciemny");
-}
+const polecane = [
+    { id: 730, nazwa: "Counter-Strike: Global Offensive", cena: "Darmowa" },
+    { id: 570, nazwa: "Dota 2", cena: "Darmowa" },
+    { id: 440, nazwa: "Team Fortress 2", cena: "Darmowa" },
+    { id: 1091500, nazwa: "Cyberpunk 2077", cena: "199,99 zł" },
+    { id: 1174180, nazwa: "Red Dead Redemption 2", cena: "249,99 zł" },
+    { id: 271590, nazwa: "Grand Theft Auto V", cena: "99,99 zł" },
+    { id: 292030, nazwa: "The Witcher 3: Wild Hunt", cena: "89,99 zł" },
+    { id: 578080, nazwa: "PLAYERUNKNOWN'S BATTLEGROUNDS", cena: "89,99 zł" }
+]
 
-MotywPrzycisk.addEventListener("click", ZmienMotyw);
-
-// Przechwytywanie i wstawianie banerów z zewnętrznej strony.
-function ZwrocBaner(id) {
-    let baner = `https://cdn.akamai.steamstatic.com/steam/apps/${id}/capsule_616x353.jpg`;
-    let response = `<a href="#" class="baner">
-            <img src="${baner}" alt="Baner ${id}">
-        </a>`;
-    return response;
-}
-
-function WstawBanery() {
-    let kod = ZwrocBaner(730) + ZwrocBaner(570) + ZwrocBaner(440);
-    document.getElementById("banery-reklamowe").innerHTML = kod;
-
-}
-document.addEventListener('DOMContentLoaded', WstawBanery);
-
-function ZwrocOkladke(id) {
-    let okladka = `https://cdn.akamai.steamstatic.com/steam/apps/${id}/library_600x900.jpg`;
-    return okladka;
-}
-
-// Optymalizacja/minimalizacja HTML
-
-// Atrybuty rozwijanego menu
 const menuRozwijaneAtrybut = [
     { href: "#", icon: "fa-circle-user", text: "Konto"},
     { href: "#", icon: "fa-basket-shopping", text: "Koszyk"},
@@ -45,25 +24,85 @@ const menuAtrybut = [
     { href: "#", icon: "fa-hand-holding-dollar", text: "Prepaid" },
 ];
 
+function renderBody() {
+    const body = document.getElementById("body");
+    body.innerHTML = `
+        <header class="glowny-header">
+            <div class="pasek-gorny">
+                <div class="kontener">
+                    <a href="index.html" class="przekierowanie-mainpage">
+                        <img src="logo.png" href="index.html" width="80" height="80" alt="GameStation - Strona Główna"
+                            class="logo-img">
+                    </a>
+                    <form class="wyszukiwarka">
+                        <input type="text" placeholder="Wyszukaj..">
+                        <button type="submit">Szukaj</button>
+                    </form>
+                    <div class="konto-menu">
+                        <button><span>User</span></button>
+                        <div class="rozwijane-menu" id="rozwijane-menu"></div>
+                    </div>
+                    <div class="konto-menu">
+                        <Button id="motyw-przycisk"><span>Motyw</span></Button>
+                    </div>
+                </div>
+            </div>
+            <div class="pasek-dolny">
+                <div class="kontener">
+                    <div class="kafelki">
+                        <ul id="menu-dolne"></ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div id="banery-reklamowe" class="banery-reklamowe">
+        </div>
+        <div class="jasna-kategoria">
+            <div class="kategoria" id="">
+                <h2>Polecane</h2>
+                <div class="produkty" id="polecane"></div> 
+            </div>
+        </div>
+        <div class="ciemna-kategoria">
+            <div class="kategoria" id=""></div>
+        </div>
+        <div class="kontener">
+            <div class="naglowek">
+                <h1>Taniej gier nie znajdziesz, nie no stary mówie ci.</h1>
+                <h2>No chyba, że wujek Gaben powie inaczej...</h2>
+            </div>
+        </div>
+
+        <footer class="stopka">
+            Copyright © 2026 GameStation. Wszelkie prawa zastrzeżone.
+        </footer>
+    `;
+}
+
+// Przechwytywanie i wstawianie banerów z zewnętrznej strony.
+function ZwrocBaner(id) {
+    let baner = `https://cdn.akamai.steamstatic.com/steam/apps/${id}/capsule_616x353.jpg`;
+    return `<a href="#" class="baner"><img src="${baner}" alt="Baner ${id}"></a>`;
+}
+
+function WstawBanery() {
+    let kod = ZwrocBaner(730) + ZwrocBaner(570) + ZwrocBaner(440);
+    document.getElementById("banery-reklamowe").innerHTML = kod;
+}
+
+function ZwrocOkladke(id) { 
+    return `https://cdn.akamai.steamstatic.com/steam/apps/${id}/library_600x900.jpg`;
+}
+
 function WstawProdukt(id, nazwa, cena) {
     let okladka = ZwrocOkladke(id);
-    let response = `<div class="produkt">
-                    <img src="${okladka}" alt="Cyberpunk 2077">
-                    <h3>${nazwa}</h3>
-                    <p class="cena">${cena}</p>
-                </div>`;
-    return response;
+    return `<div class="produkt">
+                <img src="${okladka}" alt="Cyberpunk 2077">
+                <h3>${nazwa}</h3>
+                <p class="cena">${cena}</p>
+            </div>`;
 }
-let polecane = [
-    { id: 730, nazwa: "Counter-Strike: Global Offensive", cena: "Darmowa" },
-    { id: 570, nazwa: "Dota 2", cena: "Darmowa" },
-    { id: 440, nazwa: "Team Fortress 2", cena: "Darmowa" },
-    { id: 1091500, nazwa: "Cyberpunk 2077", cena: "199,99 zł" },
-    { id: 1174180, nazwa: "Red Dead Redemption 2", cena: "249,99 zł" },
-    { id: 271590, nazwa: "Grand Theft Auto V", cena: "99,99 zł" },
-    { id: 292030, nazwa: "The Witcher 3: Wild Hunt", cena: "89,99 zł" },
-    { id: 578080, nazwa: "PLAYERUNKNOWN'S BATTLEGROUNDS", cena: "89,99 zł" }
-]
+
 function WstawProdukty(lista) {
     let kod = "";
     for (let i = 0; i < 5; i++) {
@@ -71,12 +110,11 @@ function WstawProdukty(lista) {
     }
     return kod;
 }
-function WstawPolecane(polecane) {
-    document.getElementById("polecane").innerHTML = WstawProdukty(polecane);
-}
 
 // Renderowanie na stronie
 document.addEventListener("DOMContentLoaded", () => {
+    
+    renderBody();
     
     const menuRozwijane = document.getElementById("rozwijane-menu");
     if (menuRozwijane) {
@@ -99,6 +137,15 @@ document.addEventListener("DOMContentLoaded", () => {
             </li>
         `).join("");
     }
-    WstawPolecane(polecane);
+    
+    WstawBanery();
+    document.getElementById("polecane").innerHTML = WstawProdukty(polecane);
+    
+    const MotywPrzycisk = document.getElementById("motyw-przycisk")
+    if (MotywPrzycisk) {
+        MotywPrzycisk.addEventListener("click", () => {
+            document.body.classList.toggle("tryb-ciemny")
+        });
+    }
 });
 
